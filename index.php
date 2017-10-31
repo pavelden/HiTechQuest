f069e245ecaf5d4e5bffb8e00d39920ad2932834
Orel45rus <Pahan.com1998@mail.ru>
2017-10-31 21:32:39 +0500

    Сделал меню и навёл порядок

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>HiTech Квесты  - Добро пожаловать!</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>HiTech Квесты  - Добро пожаловать!</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<!--Подуключение верхней линии-->
-	<?php require("headline.php"); ?>
-	<!--Подключение левого меню-->
-	<?php require("menu.php"); ?>
-	<!--Подключение баннера-->
-	<?php require("banner.php"); ?>
-	<!--Подключение информ файла-->
-	<?php require("info.php"); ?>
+    <!--Подуключение верхней линии-->
+    <?php require("templates/headline.php"); ?>
+    <!--Подключение левого меню-->
+    <?php require("templates/menu.php"); ?>
+    <!--Подключение баннера-->
+    <?php require("templates/banner.php"); ?>
+    <!--Подключение информ файла-->
+    <?php require("info/main.php"); ?>
 </body>
 </html>
---
+++ templates/menu.php
@@ -0,0 +1,28 @@
+<div class="menu">
+    <div class="menulogo">
+        <a href="index.php"><div class="logo"></div>
+        <p class="underlogo">Квесты в реальности</p></a>
+        <div class="categories">
+            <div id="dropdown" class="category">
+                КВЕСТЫ >
+                <ul class="dropdown">
+                    <li><a href="#">Лабиринт</a></li>
+                    <li><a href="#">Роботы</a></li>
+                    <li><a href="#">Хакеры</a></li>
+                </ul>
+            <script src="../js/dropdown.js"></script>
+            </div>
+            <div class="category">БЛОГ ></div>
+            <div class="category">ОБРАТНАЯ СВЯЗЬ ></div>
+	</div>
+        <div class="social">
+            <a href="http://vk.com/"><img src="img/social/vk.png"></a>
+            <a href="http://ok.ru/"><img src="img/social/ok.png"></a>
+            <a href="http://twitter.com/"><img src="img/social/twitter.png"></a>
+            <a href="http://instagram.com/"><img src="img/social/instagram.png"></a>
+        </div>
+        <div class="copyright">
+            ©Copyright | ООО "Горе-программисты"
+        </div>
+    </div>
+</div>
