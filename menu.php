ce811f187f929ec3ce7a517d050b426ae95dd368
secondos <secondos@secondOS.Home>
2017-10-31 13:10:08 +0500

    Скелет

---
+++ menu.php
@@ -0,0 +1,12 @@
+<div class="menu">
+	<div class="menulogo">
+		<div class="logo"></div>
+		<div class="categories">
+			<div class="category"></div>
+			<div class="category"></div>
+			<div class="category"></div>
+		</div>
+		<div class="social"></div>
+		<div class="copyright"></div>
+	</div>
+</div>
