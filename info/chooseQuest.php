fabf163b79756f42533c65484e2c80626730b81a
Orel45rus <Pahan.com1998@mail.ru>
2017-11-04 19:19:44 +0500

    Добавил слайдер, выбор квеста и общие улучшения

--- info/chooseQuest.php
+++ info/chooseQuest.php
@@ -1,1 +1,20 @@
-<h1>Выберите квест</h1>
+<div class="welcome">
+    <p>Пожалуйста, выберите квест</p>
+</div>
+<div class="quests">
+    <div class="questList">
+        <a href="?page=quest1"><img class="questImg" src="img/vr.jpg"></a>
+        <hr>
+        <a href="?page=quest1"><p>Лабиринт</p></a>
+    </div>
+    <div id="middleQuest" class="questList">
+        <a href="?page=quest2"><img class="questImg" src="img/robot.png"></a>
+        <hr>
+        <a href="?page=quest2"><p>Роботы</p></a>
+    </div>
+    <div class="questList">
+        <a href="?page=quest3"><img class="questImg" src="img/hacker.jpg"></a>
+        <hr>
+        <a href="?page=quest3"><p>Хакеры</p></a>
+    </div>
+</div>
--- templates/banner.php
+++ templates/banner.php
@@ -1,1 +1,8 @@
-<div class="banner"></div>
+<div class="banner">
+    <ul id="slides">
+        <li class="slide showing"></li>
+        <li class="slide"></li>
+        <li class="slide"></li>
+    </ul>
+</div>
+<script src="js/slider.js"></script>
